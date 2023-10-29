0fe90d0c16553cf19ffb061b66e626663c3ab1f1
Vitor Cristiano Fellizatti <111813682+Vicrisfell@users.noreply.github.com>
2023-10-29 01:35:30 -0300

    Add files via upload

---
+++ biblioteca/core/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from core.models import LivroModel
+
+
+def validate_title(value):
+    if len(value) < 3:
+        raise ValidationError('Deve ter pelo menos tres caracteres')
+
+
+class LivroForm(forms.ModelForm):
+
+    class Meta:
+        model = LivroModel
+        fields = ['titulo', 'editora', 'autor', 'isbn']
+        error_messages = {
+            'titulo': {
+                'required': ("Informe o título do livro."),
+            },
+            'editora': {
+                'required': ("Informe a editora do livro."),
+            },
+            'autor':{
+                'required': ("Informe o autor(a) do livro"),
+            },
+            'isbn':{
+                'required':("Informe o isbn do livro"),
+            },
+        }
+
+    def clean_titulo(self):
+        titulo = self.cleaned_data['titulo']
+        validate_title(titulo)
+        return titulo
+
+    def clean_editora(self):
+        editora = self.cleaned_data['editora']
+        validate_title(editora)
+        return editora
+
+    def clean_autor(self):
+        autor = self.clean_autor['autor']
+        validate_title(autor)
+        return autor
+    
+    def clean_isbn(self):
+        isbn = self.clean_isbn['isbn']
+        validate_title(isbn)
+        return isbn
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+        return self.cleaned_data
+
